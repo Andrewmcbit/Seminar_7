e02781b7b5056131926a6f616d8add8bf494ed6f
Andrew <1.smirnov.a.v.1@gmail.com>
2023-12-21 19:38:31 +0700

    первое задание

---
+++ 1/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
+Использовать рекурсию, не использовать циклы.*/
+
+void PrintNumbers(int start, int end){
+    Console.Write(start + " ");
+    if (start < end) {
+        PrintNumbers(start+1, end);
+    }
+}
+
+int M = 11;
+int N = 28;
+
+PrintNumbers(M,N);
